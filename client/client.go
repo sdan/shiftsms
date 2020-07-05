80383445078fe0840eb64d15e19089a3ee21af12
Surya <git@sdan.cc>
2020-07-05 14:42:55 -0700

    parse DMs into interfaces

--- client/client.go
+++ client/client.go
@@ -13,23 +13,29 @@ import (
 
 //Struct to parse webhook load
 type WebhookLoad struct {
-	UserId           string  `json:"for_user_id"`
-	TweetCreateEvent []Tweet `json:"tweet_create_events"`
+	UserId             string        `json:"for_user_id"`
+	DirectMessageEvent []interface{} `json:"direct_message_events"`
+	Metadata           interface{}   `json:"apps"`
 }
 
-//Struct to parse tweet
-type Tweet struct {
-	Id    int64
-	IdStr string `json:"id_str"`
-	User  User
-	Text  string
+//Struct to parse DM
+type DirectMessage struct {
+	Id          int
+	Type        string
+	RecipientID User
+	SenderID    User
+	Message     string
+}
+
+type Metadata struct {
+	User1 User
+	User2 User
 }
 
 //Struct to parse user
 type User struct {
-	Id     int64
-	IdStr  string `json:"id_str"`
-	Name   string
+	Id     int
+	Name   string `json:"name"`
 	Handle string `json:"screen_name"`
 }
 
@@ -80,27 +86,27 @@ func SubscribeWebhook() {
 	}
 }
 
-func SendTweet(tweet string, reply_id string) (*Tweet, error) {
-	fmt.Println("Sending tweet as reply to " + reply_id)
-	//Initialize tweet object to store response in
-	var responseTweet Tweet
-	//Add params
-	params := url.Values{}
-	params.Set("status", tweet)
-	params.Set("in_reply_to_status_id", reply_id)
-	//Grab client and post
-	client := CreateClient()
-	resp, err := client.PostForm("https://api.twitter.com/1.1/statuses/update.json", params)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	//Decode response and send out
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
-	err = json.Unmarshal(body, &responseTweet)
-	if err != nil {
-		return nil, err
-	}
-	return &responseTweet, nil
-}
+// func SendTweet(tweet string, reply_id string) (*Tweet, error) {
+// 	fmt.Println("Sending tweet as reply to " + reply_id)
+// 	//Initialize tweet object to store response in
+// 	var responseTweet Tweet
+// 	//Add params
+// 	params := url.Values{}
+// 	params.Set("status", tweet)
+// 	params.Set("in_reply_to_status_id", reply_id)
+// 	//Grab client and post
+// 	client := CreateClient()
+// 	resp, err := client.PostForm("https://api.twitter.com/1.1/statuses/update.json", params)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+// 	//Decode response and send out
+// 	body, _ := ioutil.ReadAll(resp.Body)
+// 	fmt.Println(string(body))
+// 	err = json.Unmarshal(body, &responseTweet)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &responseTweet, nil
+// }
